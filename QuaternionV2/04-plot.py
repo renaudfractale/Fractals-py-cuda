9ee6755e0535a1e4da24138be6500ee95e4420f0
Novel-Conseil <cloud@novelconseil.com>
2023-04-22 03:41:19 +0200

    Update 04-plot.py

--- QuaternionV2/04-plot.py
+++ QuaternionV2/04-plot.py
@@ -7,7 +7,7 @@ from matplotlib.ticker import PercentFormatter
 import Libs
 import copy
 
-PathFile= "2023-04-19_18-41-34.0262410-Filter.npz"
+PathFile= "2023-04-19_18-41-34.0262410.npz"
 
 Data = Libs.OpenFileNpz(PathFile)
 
@@ -19,24 +19,107 @@ Parameter = Libs.Json2Objet(JsonString)
 
 lenPlotEnd = 35
 lenPlotStart = 30
+
+
+
 #print("i+lenHistostart =  "+str(type(i+lenHistostart)))
-filter_arr = tab_Source[(tab_Source  >  (lenPlotStart-1)) & (tab_Source < (lenPlotEnd+1))]
+##
 #filter_arr = tab_Source.any >  lenPlotStart-1 and tab_Source.any < lenPlotEnd+1
-print("filter_arr type " + str(type(filter_arr)))
+#print("filter_arr type " + str(type(filter_arr)))
+Temp = tab_Source[(tab_Source  >  (lenPlotStart-1)) & (tab_Source < (lenPlotEnd+1))]
+print(Temp.size)
+X = np.zeros(Temp.size,np.float16)
+Y = np.zeros(Temp.size,np.float16)
+Z = np.zeros(Temp.size,np.float16)
+C = np.zeros(Temp.size,np.int16)
 
 tab_Index= np.arange(0,tab_Source.size)
+#intX = np.trunc(tab_Index / (Parameter["nbPoint"]*Parameter["nbPoint"]))
+#intY = np.trunc((tab_Index % (Parameter["nbPoint"]*Parameter["nbPoint"]))/Parameter["nbPoint"])
+#intZ = tab_Index % Parameter["nbPoint"]
+#print("*******************")
+#print(max(intX))
+#print(min(intX))
+#print("*******************")
+#print("*******************")
+#print(max(intY))
+#print(min(intY))
+#print("*******************")
+#print("*******************")
+#print(max(intZ))
+#print(min(intZ))
+#print("*******************")
+Arange = range(lenPlotStart,lenPlotEnd+1)
+index = -1
+for i in Arange:
+    filter_arr = tab_Source ==  i
+    tab_IndexF = tab_Index[filter_arr]
+    Xint = np.trunc(tab_IndexF / (Parameter["nbPoint"]*Parameter["nbPoint"]))
+    Yint = np.trunc((tab_IndexF % (Parameter["nbPoint"]*Parameter["nbPoint"]))/Parameter["nbPoint"])
+    Zint = tab_IndexF % Parameter["nbPoint"]
+    print("i = "+str(i))
+    print("size = "+str(tab_IndexF.size))    
+    #print(Xint)
+    #print(Yint)
+    print(Zint)
+    XFloat = np.float16(Xint)*Parameter["pasX"]/float(Parameter["nbPoint"]-1)
+    YFloat = np.float16(Yint)*Parameter["pasY"]/float(Parameter["nbPoint"]-1)
+    ZFloat = np.float16(Zint)*Parameter["pasZ"]/float(Parameter["nbPoint"]-1)
+    #print(XFloat)
+    #print(YFloat)
+    #print(ZFloat)
+    #print(max(ZFloat))
+    #print(min(ZFloat))
+    for k in np.arange(0,tab_IndexF.size):
+        index+=1
+        X[index] =  XFloat[k]
+        Y[index] =  YFloat[k]
+        Z[index] =  ZFloat[k]
+        C[index] = i
 
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(X, Y, Z,C,C)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
+"""
+i=-1
+k=-1
+for source in tab_Source:
+    i+=1
+    if(source>(lenPlotStart-1) and source < (lenPlotEnd+1)):
+        k+=1
+        Xint= int((i-(i % (Parameter["nbPoint"]*Parameter["nbPoint"])))/(Parameter["nbPoint"]*Parameter["nbPoint"]))
+        print("i = "+str(i))
+        print("Xint = "+str(Xint))
+        print("-----------------")
+        
+
+
+
+tab_Index= np.arange(0,tab_Source.size)
 tab_SourceF = tab_Source[filter_arr]
 tab_IndexF = tab_Index[filter_arr]
 print("tab_IndexF.size = " + str(tab_IndexF.size))
-X = np.zeros(tab_IndexF.size,np.float16)
+Xint = (tab_IndexF % (Parameter["nbPoint"]*Parameter["nbPoint"]))
 Y = np.zeros(tab_IndexF.size,np.float16)
 Z = np.zeros(tab_IndexF.size,np.float16)
 C = np.zeros(tab_IndexF.size,np.int16)
-
-
+print(max(Xint))
+print(tab_IndexF)
+print(tab_SourceF)
+print(filter_arr)
+print(tab_Source)
+print(tab_Index[tab_Source.size-1])
+print(tab_IndexF[tab_IndexF.size-1])
+"""
 
 """
+
 histo= np.zeros(21,np.int32)
 for i in nbRange:
     histo[tab_int[i]]+=1
@@ -52,6 +135,7 @@ dimw = w / dim
 ax.hist(tab_int, bins=20)
 
 plt.show()
+"""
 """
 j=0
 for i in nbRange:
@@ -81,3 +165,4 @@ ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
 plt.show()
+"""
